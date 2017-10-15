67238e72c4b4bc9398bfe3dcbd5fddbf2961b999
Hide Asa <asakura@asasoft.info>
2017-10-15 11:17:46 +0000

    #25 save Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.create(commnet_params)
+    redirect_to post_path(@post)
+  end
+  
+  private
+    def commnet_params
+      params.require(:comment).permit(:body)
+    end
 end
